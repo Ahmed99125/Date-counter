663c3e91134fcb1c0d80cbd3d8ab952cc2d7beb4
Ahmed99125 <at99125@gmail.com>
2024-06-14 03:56:47 +0300

    uploading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="counters">
+        <Counter
+          name="Step"
+          state={step}
+          setState={setStep}
+          limitDec={1}
+          step={1}
+        />
+        <Counter
+          name="Count"
+          state={count}
+          setState={setCount}
+          limitDec={null}
+          step={step}
+        />
+      </div>
+      <ShowDate count={count} />
     </div>
+  );
+}
+
+function Counter({ name, state, setState, limitDec, step }) {
+  const handleDec = () => {
+    if (limitDec === null || state > limitDec) setState((cur) => cur - step);
+  };
+
+  const handleInc = () => setState((cur) => cur + step);
+
+  return (
+    <div className="counter">
+      <button onClick={handleDec}>-</button>
+      <span>
+        {name}: {state}
+      </span>
+      <button onClick={handleInc}>+</button>
+    </div>
+  );
+}
+
+function ShowDate({ count }) {
+  let time = new Date();
+  time.setDate(time.getDate() + count);
+  return (
+    <p className="date">
+      {count === 0 && "Today is "}
+      {count < 0 && `${-count} days ago was `}
+      {count > 0 && `${count} days from today is `}
+      {time.toDateString()}
+    </p>
   );
 }
 
